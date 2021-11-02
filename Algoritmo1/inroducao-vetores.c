b0551a321b0efd67af27ba75930f6e23709510c9
Pâmela Resende <pamelaschavesresende@gmail.com>
2021-11-02 19:37:57 -0300

    Add new exercises

---
+++ Algoritmo1/inroducao-vetores.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int vet[5];
+    for(int i=0; i<=4; i++){
+        vet[i]=5;
+    }
+    for(int i=0; i<=4; i++){
+        printf("%d", vet[i]);
+    }
+}
+
